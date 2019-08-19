b0c7fab734a7e947f70e7d19422bbef62fc2a381
georgevcr2 <32660775+georgevcr2@users.noreply.github.com>
2019-08-19 15:10:51 -0400

    First commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Header.css";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="component-header">
+        <img
+          src=""
+          width="32"
+          height="32"
+          alt="temp"
+        />
+        Waifu/Husbando Search
+        <img
+          src=""
+          width="32"
+          height="32"
+          alt="temp"
+        />
+      </header>
+    );
+  }
+}
